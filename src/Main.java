8ab99ad32326ea8761724e706a99c8131bca3594
Vladimir Semenov <osmyswait@gmail.com>
2022-10-22 12:05:18 +0300

    changed

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 1000; // стоимость билета
+        int oneMile = 20; // стоимость одой мили
+
+
+        System.out.println("За данную поездку начислено");
+        System.out.println((ticketCost / oneMile) + "миль");
+    }
+}
